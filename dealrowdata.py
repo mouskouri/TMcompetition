149e9277f36bc24aa976758980df5b900fa84abc
mouskouri <875021365@qq.com>
2015-04-03 16:22:48 +0800

    the fourth submission
    加入了验证集

--- dealrowdata.py
+++ dealrowdata.py
@@ -1,15 +1,17 @@
 from csv import DictReader
 
 train_user = 'train_user.csv'  # path to training file
-test = 'train_item.csv'  # path to testing file
 
 user_item_label = {}
+
 user_dic = {}
 item_dic = {}
 user_dop = []
 item_dop = []
 
+validation_time = '12-17-00'
 splitime = '12-18-00'
+if_validation = True
 
 #cal user_behave_count
 def cal_abnormal(path):
@@ -41,6 +43,7 @@ def cal_abnormal(path):
 
 #bulid label dic
 def build_label(path):
+	validation_dic = {}
 
 	for t, row in enumerate(DictReader(open(path))):
 		date = row['time'].split()
@@ -51,18 +54,37 @@ def build_label(path):
 		Eid = [row['user_id'], row['item_id']]
 		Eid = ','.join(Eid)
 
-		if ((time > splitime) and (row['behavior_type'] == '4')):
-			if Eid not in user_item_label:
-				user_item_label[Eid] = 0
-			#user_item_label[Eid] += 1
+		if(if_validation):
+			if (((time > validation_time) and (time <= splitime)) and (row['behavior_type'] == '4')):
+				if Eid not in user_item_label:
+					user_item_label[Eid] = 0
 
+			if ((time > splitime) and (row['behavior_type'] == '4')):
+				if row['user_id'] not in validation_dic:
+					validation_dic[row['user_id']] = {}
+				if row['item_id'] not in validation_dic[row['user_id']]:
+					validation_dic[row['user_id']][row['item_id']] = 0
+
+		else:
+			if ((time > splitime) and (row['behavior_type'] == '4')):
+				if Eid not in user_item_label:
+					user_item_label[Eid] = 0	
+
+	if(if_validation):
+		with open('validation_buy.csv', 'w') as validation:
+			validation.write('user_id,item_id\n')
+			for user in validation_dic:
+				for item in validation_dic[user]:
+					validation.write('%s,%s\n' % (user, item))
+	
+		
 #bulid train data
 def construct_train(path):
 	count1 = 0
 	count = 0
 	drop = 0
 
-	with open('train_deal.csv', 'w') as train:
+	with open('train_deal_validation.csv', 'w') as train:
 
 		train.write('user_id,item_id,label,behavior_type,user_geohash,item_category,time\n')
 
@@ -79,7 +101,12 @@ def construct_train(path):
 			Eid = [row['user_id'], row['item_id']]
 			Eid = ','.join(Eid)
 
-			if (time <= splitime):
+			label_time = splitime
+
+			if(if_validation):
+				label_time = validation_time
+
+			if (time <= label_time):
 				count += 1
 				label = '0'
 				if (Eid in user_item_label):
@@ -95,7 +122,6 @@ def construct_train(path):
 cal_abnormal(train_user)
 build_label(train_user)
 construct_train(train_user)
-print len(item_dop)
-#print len(user_item_label)
+#print len(item_dop)
 
 
